237115fca9876d9f26522e2487761ae178bad214
gnut3ll4 <thibaut.tauveron@gmail.com>
2015-12-23 12:31:09 -0500

    Proof of concept syncadapter

--- app/src/main/java/ca/etsmtl/applets/seemobile/postulations/PostulationFragment.java
+++ app/src/main/java/ca/etsmtl/applets/seemobile/postulations/PostulationFragment.java
@@ -1,6 +1,15 @@
 package ca.etsmtl.applets.seemobile.postulations;
 
+import android.accounts.Account;
+import android.accounts.AccountManager;
+import android.content.ContentResolver;
+import android.content.Context;
+import android.content.SyncStatusObserver;
+import android.database.ContentObserver;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
 import android.support.design.widget.Snackbar;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
@@ -41,10 +50,60 @@ public class PostulationFragment extends Fragment implements PostulationView, Ad
 
         listView.setOnItemClickListener(this);
 
-
+        Bundle settings = new Bundle();
+        settings.putBoolean(ContentResolver.SYNC_EXTRAS_MANUAL, true);
+        settings.putBoolean(ContentResolver.SYNC_EXTRAS_EXPEDITED, true);
+//        Account mAccount = new Account("SEEMobile", "ca.etsmtl.applets.seemobile");
+        ContentResolver.requestSync(CreateSyncAccount(getActivity()), "ca.etsmtl.applets.seemobile.provider", settings);
+        getActivity().getContentResolver().registerContentObserver(
+                Uri.parse("content://ca.etsmtl.applets.seemobile.provider"),
+                false,
+                new ContentObserver(new Handler(Looper.getMainLooper())) {
+                    public void onChange(boolean selfChange) {
+                        int i = 0;
+                    }
+                });
         return view;
 
 
+    }
+
+    // The authority for the sync adapter's content provider
+    public static final String AUTHORITY = "ca.etsmtl.applets.seemobile.provider";
+    // An account type, in the form of a domain name
+    public static final String ACCOUNT_TYPE = "ca.etsmtl.applets.seemobile";
+    // The account name
+    public static final String ACCOUNT = "SEEMobile";
+
+    /**
+     * Create a new dummy account for the sync adapter
+     *
+     * @param context The application context
+     */
+    public static Account CreateSyncAccount(Context context) {
+        // Create the account type and default account
+        Account newAccount = new Account(
+                ACCOUNT, ACCOUNT_TYPE);
+        // Get an instance of the Android account manager
+        AccountManager accountManager = AccountManager.get(context);
+        /*
+         * Add the account and account type, no password or user data
+         * If successful, return the Account object, otherwise report an error.
+         */
+        if (accountManager.addAccountExplicitly(newAccount, null, null)) {
+            /*
+             * If you don't set android:syncable="true" in
+             * in your <provider> element in the manifest,
+             * then call context.setIsSyncable(account, AUTHORITY, 1)
+             * here.
+             */
+        } else {
+            /*
+             * The account exists or some other error occurred. Log this, report it,
+             * or handle it internally.
+             */
+        }
+        return newAccount;
     }
 
     @Override
