ad08c610f0381e0725233956a7212c7de04e2cd0
gnut3ll4 <thibaut.tauveron@gmail.com>
2015-12-24 05:27:45 -0500

    Added support lambdas

--- app/src/main/java/ca/etsmtl/applets/seemobile/presenter/PostulationPresenter.java
+++ app/src/main/java/ca/etsmtl/applets/seemobile/presenter/PostulationPresenter.java
@@ -1,14 +1,10 @@
 package ca.etsmtl.applets.seemobile.presenter;
 
-import java.util.List;
-
 import javax.inject.Inject;
 
 import ca.etsmtl.applets.seemobile.Injector;
-import ca.etsmtl.applets.seemobile.model.Postulation;
 import ca.etsmtl.applets.seemobile.service.SEEService;
 import ca.etsmtl.applets.seemobile.view.PostulationView;
-import rx.Observer;
 import rx.android.schedulers.AndroidSchedulers;
 import rx.schedulers.Schedulers;
 
@@ -35,22 +31,9 @@ public class PostulationPresenter implements IPostulationPresenter {
                 .getPostulations()
                 .subscribeOn(Schedulers.newThread())
                 .observeOn(AndroidSchedulers.mainThread())
-                .subscribe(new Observer<List<Postulation>>() {
-                    @Override
-                    public void onCompleted() {
-
-                    }
-
-                    @Override
-                    public void onError(Throwable e) {
-
-                    }
-
-                    @Override
-                    public void onNext(List<Postulation> postulations) {
-                        postulationView.setItems(postulations);
-                        postulationView.hideProgress();
-                    }
+                .subscribe(postulations -> {
+                    postulationView.setItems(postulations);
+                    postulationView.hideProgress();
                 });
 
     }
